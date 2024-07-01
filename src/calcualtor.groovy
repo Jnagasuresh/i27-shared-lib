b934a172180e478da039c242e97921e6696264bc
Nags <jnagasuresh@hotmail.om>
2024-07-01 10:26:48 +0530

    shared library implementation

--- src/calcualtor.groovy
+++
@@ -1,1 +0,0 @@
-package com.i27academy.builds
---
+++ src/com/i27academy/builds/Dcoker.groovy
@@ -0,0 +1,17 @@
+package com.i27academy.builds
+
+class Docker {
+    def jenkins
+    Docker(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    // Applicaiton Build
+
+    // Docker Build
+
+    // Docker login
+
+    // Docker Push
+    
+}

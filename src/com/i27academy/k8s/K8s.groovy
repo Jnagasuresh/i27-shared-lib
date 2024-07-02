163063020866842041b78306f71fcdcaa617e51b
Nags <jnagasuresh@hotmail.om>
2024-07-02 16:10:29 +0530

    authentication

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -9,6 +9,7 @@ class K8s {
     def auth_login(){
         jenkins.sh """
         echo "Entering into Kubernetes Authentication/Login Method"
+        gcloud config set account jenkins@regal-cycling-424510-r6.iam.gserviceaccount.com
         # gcloud auth activate-service-account jenkins@regal-cycling-424510-r6.iam.gserviceaccount.com --key-file=key.json
         gcloud compute instances list
         gcloud container clusters get-credentials cart-cluster --zone us-west1-a --project regal-cycling-424510-r6

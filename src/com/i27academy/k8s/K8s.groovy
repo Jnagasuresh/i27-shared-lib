ecb1c8e938942fa52b923631da1d2507d6a45f68
Nags <jnagasuresh@hotmail.om>
2024-07-03 13:28:49 +0530

    kubectl test

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -12,7 +12,7 @@ class K8s {
         sudo gcloud compute instances list      
         sudo gcloud container clusters get-credentials cart-cluster --zone us-west1-a --project regal-cycling-424510-r6  
         echo "******************* Get Nodes in the Cluster ********************************"
-        sudo get nodes
+        sudo kubectl get nodes
         """
     }
 }
